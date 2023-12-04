4b5892c3a76efc0318aeda8f1b90ee108f612cb4
Run <run2gotv@gmail.com>
2023-12-04 19:42:42 +0100

    adjusted test script

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -9,36 +9,36 @@ require('dotenv').config({ path: path.resolve(__dirname, '../config.ini') });
 const serverPort = process.env.SERVER_PORT;
 
 describe('Server Test', () => {
-  let serverProcess;
+    let serverProcess;
 
-  // Start the server before running tests
-  beforeAll((done) => {
+    // Start the server before running tests
+    beforeAll((done) => {
     serverProcess = exec(`node server.js --port=${serverPort}`, (error, stdout, stderr) => {
-      // Log the output of the child process
-      console.log('Child Process Output:', stdout);
-      console.error('Child Process Error:', stderr);
+        // Log the output of the child process
+        console.log('Child Process Output:', stdout);
+        console.error('Child Process Error:', stderr);
 
-      // If there was an error, log it and proceed
-      if (error) {
+        // If there was an error, log it and proceed
+        if (error) {
         console.error('Child Process Error:', error);
-      }
+        }
 
-      done(); // Continue with the test suite
+        done(); // Continue with the test suite
     });
 
     setTimeout(() => done(), 3000); // Wait 3 seconds for the server to start
-  });
+    });
 
-  // Testing server functionality
-  test('Server responds to GET request', async () => {
-    const response = await request(`http://localhost:${serverPort}`).get('/');
+    // Testing server functionality
+    test('Server responds to GET request', async () => {
+        const response = await request(`http://localhost:${serverPort}`).get('/');
 
-    // Expect a status code of 200 for a successful request
-    expect(response.status).toBe(200);
-  });
+        // Expect a status code of either 302 or 200
+        expect(response.status).toBeOneOf([302, 200]);
+    });
 
-  // Stop the server after running tests
-  afterAll(() => {
-    serverProcess.kill('SIGTERM');
-  });
+    // Stop the server after running tests
+    afterAll(() => {
+        serverProcess.kill('SIGTERM');
+    });
 });

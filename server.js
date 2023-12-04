2674c0e8fe644bbc6f8b1fa6b8e1c16f186e62ad
Run <run2gotv@gmail.com>
2023-12-04 12:28:47 +0100

    #10 add express.js

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 // server.js
 
+const express = require('express');
+const app = express();
+app.get('*', (reg, res) => {
+    res.redirect("https://github.com");
+  });
+
 console.log(`GateKeeper started`);

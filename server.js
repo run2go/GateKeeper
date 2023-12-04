0e031492c1d9fa34d86c30571426282102e23fdc
Run <run2gotv@gmail.com>
2023-12-04 14:40:52 +0100

    #4 graceful shutdown function added

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 // server.js
 
+const serverName = "GateKeeper"
+const redirectURL = "https://github.com";
+
 const express = require('express');
 const app = express();
 app.get('*', (reg, res) => {
-    res.redirect("https://github.com");
+    res.redirect(redirectURL);
   });
 
-console.log(`GateKeeper started`);
+console.log(`${serverName} started`);
+
+// Handle shutdown signals
+process.on('SIGTERM', () => {
+    gracefulShutdown();
+  });
+process.on('SIGINT', () => {
+    gracefulShutdown();
+});
+  
+function gracefulShutdown() {
+    console.log(`${serverName} stopped`);
+
+    // Exit the process
+    process.exit(0);
+
+    // Force shutdown if server hasn't stopped in time
+    setTimeout(() => {
+        console.error(`${serverName} terminated`);
+        process.exit(22);
+    }, 5000); // 5 seconds
+}

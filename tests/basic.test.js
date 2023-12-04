bddf131fbfddb38745be1cbe218cb84be6b7a6d3
Run <run2gotv@gmail.com>
2023-12-04 19:49:21 +0100

    added 10s test timeout

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -8,25 +8,28 @@ const request = require('supertest');
 require('dotenv').config({ path: path.resolve(__dirname, '../config.ini') });
 const serverPort = process.env.SERVER_PORT;
 
+// Set a timeout of 10 seconds for the entire test suite
+jest.setTimeout(10000);
+
 describe('Server Test', () => {
     let serverProcess;
 
     // Start the server before running tests
-    beforeAll((done) => {
-    serverProcess = exec(`node server.js --port=${serverPort}`, (error, stdout, stderr) => {
-        // Log the output of the child process
-        console.log('Child Process Output:', stdout);
-        console.error('Child Process Error:', stderr);
+    beforeAll(async (done) => {
+        serverProcess = await exec(`node server.js --port=${serverPort}`, (error, stdout, stderr) => {
+            // Log the output of the child process
+            console.log('Child Process Output:', stdout);
+            console.error('Child Process Error:', stderr);
 
-        // If there was an error, log it and proceed
-        if (error) {
-        console.error('Child Process Error:', error);
-        }
+            // If there was an error, log it and proceed
+            if (error) {
+                console.error('Child Process Error:', error);
+            }
 
-        done(); // Continue with the test suite
-    });
+            done(); // Continue with the test suite
+        });
 
-    setTimeout(() => done(), 3000); // Wait 3 seconds for the server to start
+        setTimeout(() => done(), 2000); // Wait 2 seconds for the server to start
     });
 
     // Testing server functionality
@@ -34,7 +37,7 @@ describe('Server Test', () => {
         const response = await request(`http://localhost:${serverPort}`).get('/');
 
         // Expect a status code of either 302 or 200
-        expect(response.status).toBeOneOf([302, 200]);
+        expect([302, 200]).toContain(response.status);
     });
 
     // Stop the server after running tests

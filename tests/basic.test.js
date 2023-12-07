7eaf8bcb1367da0754218a8cfe56db9321ce9610
Run <run2gotv@gmail.com>
2023-12-07 16:38:20 +0100

    #28 #21 #7 #25

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -7,7 +7,6 @@ const path = require('path');
 // Access parameters in the config.ini file
 require('dotenv').config({ path: path.resolve(__dirname, '../config.ini') });
 const serverPort = process.env.SERVER_PORT;
-const apiEndpoint = process.env.API_ENDPOINT;
 
 // Timeout for the entire test suite
 jest.setTimeout(15000); // 15 seconds
@@ -44,25 +43,71 @@ describe('Server Test', () => {
         expect([302, 200]).toContain(response.status);
     });
 
-    // Testing server POST functionality
-    test('Server responds to invalid POST request', async () => {
-        const postData = {
-            username: 'test_user',
-            password: 'test_pw',
+	// Testing server POST functionality
+    test('Server responds to various POST requests', async () => {
+        // Helper function to make a POST request with a given payload
+        const makePostRequest = async (payload) => {
+            return await request(`http://localhost:${serverPort}`)
+                .post('/')
+                .send(payload);
         };
 
-        const response = await request(`http://localhost:${serverPort}`)
-            .post(apiEndpoint)
-            .send(postData);
-
-        // Using supertest's expect to assert the status directly
-        expect(response.status).toEqual(500);
-        // Assert the response body
-        expect(response.body).toEqual({
-            success: false,
-            error: expect.stringContaining('Invalid username'),
+        // Test case 1: List command
+        const listResponse = await makePostRequest({
+            username: 'test_admin',
+            password: 'test_pass',
+            cmd: 'list',
         });
-    });
+        expect(listResponse.body.success).toBe(true);
+
+        // Test case 2: Create user
+        const createUserResponse = await makePostRequest({
+            username: 'test_admin',
+            password: 'test_pass',
+            create: { username: 'test_user', password: 'test_pw' },
+        });
+        expect(createUserResponse.body.success).toBe(true);
+
+        // Test case 3: Create admin user
+        const createAdminUserResponse = await makePostRequest({
+            username: 'test_admin',
+            password: 'test_pass',
+            create: { username: 'temp_admin', password: 'test_pw', admin: true },
+        });
+        expect(createAdminUserResponse.body.success).toBe(true);
+
+        // Test case 4: Read user
+        const readUserResponse = await makePostRequest({
+            username: 'test_admin',
+            password: 'test_pass',
+            read: { username: 'test_user' },
+        });
+        expect(readUserResponse.body.success).toBe(true);
+
+        // Test case 5: Update user password
+        const updateUserResponse = await makePostRequest({
+            username: 'test_admin',
+            password: 'test_pass',
+            update: { username: 'test_user', password: 'new_password' },
+        });
+        expect(updateUserResponse.body.success).toBe(true);
+
+        // Test case 6: Delete user
+        const deleteUserResponse = await makePostRequest({
+            username: 'test_admin',
+            password: 'test_pass',
+            delete: { username: 'test_user' },
+        });
+        expect(deleteUserResponse.body.success).toBe(true);
+
+        // Test case 7: Attempt to delete admin
+        const deleteNonExistentUserResponse = await makePostRequest({
+            username: 'test_admin',
+            password: 'test_pass',
+            delete: { username: 'temp_admin' },
+        });
+        expect(deleteNonExistentUserResponse.body.success).toBe(false);
+	});
 
     // Stop the server after running tests
     afterAll((done) => {

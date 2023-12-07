466c40e875cc1ea8e4d74874329c55a26fa3ca24
Run <run2gotv@gmail.com>
2023-12-07 17:41:45 +0100

    run the server as an async function

--- server.js
+++ server.js
@@ -12,106 +12,113 @@ const helpURL = process.env.HELP_URL;
 // Use the timestamp methods inside the utility.js file
 const console = require('./utility');
 
-// Make use of the express.js framework for the core application
-const express = require('express');
-const app = express();
 
-// Middleware to parse JSON in request body
-app.use(express.json());
 
-// Notify that the server has been started
-console.log(`${serverName} started`);
+async function startServer(){
+	// Make use of the express.js framework for the core application
+	const express = require('express');
+	const app = express();
 
-// Bind the server to the specified port
-const server = app.listen(serverPort, () => {
-    console.log(`Now listening on port ${serverPort}`);
-});
+	// Middleware to parse JSON in request body
+	app.use(express.json());
 
-// Require the db.js file to access the sequelize functionality
-const db = require('./db');
+	// Notify that the server has been started
+	console.log(`${serverName} started`);
 
-// Fetch the latest list of valid usernames from the database
-let userList = await db.getUserList();
-console.log(`Fetched userlist: ${userList}`);
+	// Bind the server to the specified port
+	const server = app.listen(serverPort, () => {
+		console.log(`Now listening on port ${serverPort}`);
+	});
 
-// Basic functionality to forward requests on the root directory
-app.get("/", (req, res) => {
-    res.redirect(redirectURL);
-    console.log(`Server has been accessed with these parameters: ${JSON.stringify(req.params)}`);
-});
+	// Require the db.js file to access the sequelize functionality
+	const db = require('./db');
+	
+	// Fetch the latest list of valid usernames from the database
+	let userList = await db.getUserList();
+	console.log(`Fetched userlist: ${userList}`);
 
-// Handle GET requests for the API endpoint
-app.get(apiEndpoint, (req, res) => {
-    res.redirect((apiEndpoint !== "*" && apiEndpoint !== "/") ? serverURL : redirectURL); // Prevent redirect looping
-    console.log(`API has been accessed with these parameters: ${JSON.stringify(req.params)}`);
-});
+	// Basic functionality to forward requests on the root directory
+	app.get("/", (req, res) => {
+		res.redirect(redirectURL);
+		console.log(`Server has been accessed with these parameters: ${JSON.stringify(req.params)}`);
+	});
 
-// Handle POST requests for the API endpoint
-app.post(apiEndpoint, async (req, res) => {
-    const receivedData = req.body;
+	// Handle GET requests for the API endpoint
+	app.get(apiEndpoint, (req, res) => {
+		res.redirect((apiEndpoint !== "*" && apiEndpoint !== "/") ? serverURL : redirectURL); // Prevent redirect looping
+		console.log(`API has been accessed with these parameters: ${JSON.stringify(req.params)}`);
+	});
 
-    try {
-        // Check if the provided username is in the list of valid usernames
-        if (!userList.includes(receivedData.username)) {
-            throw new Error('Invalid username');
-        }
-		
-		// Authenticate the given credentials if valid
-		const authCheck = await db.auth(receivedData.username, receivedData.password);
-		if (authCheck) {
-			let result;
-			switch (true) {
-				// Command
-				case Boolean(receivedData.cmd):
-					switch (receivedData.cmd) {
-							case "help": result = helpURL; break;
-							case "list": result = userList; break;
-							default: throw new Error('Invalid command');
-					}
-					break;
-					
-				// Create
-				case Boolean(receivedData.create):
-					result = await db.dataCreate(
-						receivedData.create.username,
-						receivedData.create.password
-					);
-					userList = await db.getUserList(); // Get the current userlist
-					break;
+	// Handle POST requests for the API endpoint
+	app.post(apiEndpoint, async (req, res) => {
+		const receivedData = req.body;
 
-				// Read
-				case Boolean(receivedData.read):
-					result = await db.dataRead(receivedData.read.username);
-					break;
-
-				// Update
-				case Boolean(receivedData.update):
-					result = await db.dataUpdate(
-						receivedData.update.username,
-						receivedData.update.password
-					);
-					break;
-
-				// Delete
-				case Boolean(receivedData.delete):
-					result = await db.dataRemove(receivedData.delete.username);
-					userList = await db.getUserList(); // Get the current userlist
-					break;
-
-				default:
-					throw new Error('Invalid request format');
+		try {
+			// Check if the provided username is in the list of valid usernames
+			if (!userList.includes(receivedData.username)) {
+				throw new Error('Invalid username');
 			}
-			// Respond with the result or any appropriate response
-			res.json({ success: true, data: result });
-		}
-    } catch (error) {
-        // Handle errors
-        console.error(`Error processing POST request: ${error.message}`);
-        res.status(500).json({ success: false, error: error.message });
-    }
+			
+			// Authenticate the given credentials if valid
+			const authCheck = await db.auth(receivedData.username, receivedData.password);
+			if (authCheck) {
+				let result;
+				switch (true) {
+					// Command
+					case Boolean(receivedData.cmd):
+						switch (receivedData.cmd) {
+								case "help": result = helpURL; break;
+								case "list": result = userList; break;
+								default: throw new Error('Invalid command');
+						}
+						break;
+						
+					// Create
+					case Boolean(receivedData.create):
+						result = await db.dataCreate(
+							receivedData.create.username,
+							receivedData.create.password
+						);
+						userList = await db.getUserList(); // Get the current userlist
+						break;
 
-    console.log(`POST request handled with these params: ${JSON.stringify(receivedData)}`);
-});
+					// Read
+					case Boolean(receivedData.read):
+						result = await db.dataRead(receivedData.read.username);
+						break;
+
+					// Update
+					case Boolean(receivedData.update):
+						result = await db.dataUpdate(
+							receivedData.update.username,
+							receivedData.update.password
+						);
+						break;
+
+					// Delete
+					case Boolean(receivedData.delete):
+						result = await db.dataRemove(receivedData.delete.username);
+						userList = await db.getUserList(); // Get the current userlist
+						break;
+
+					default:
+						throw new Error('Invalid request format');
+				}
+				// Respond with the result or any appropriate response
+				res.json({ success: true, data: result });
+			}
+		} catch (error) {
+			// Handle errors
+			console.error(`Error processing POST request: ${error.message}`);
+			res.status(500).json({ success: false, error: error.message });
+		}
+
+		console.log(`POST request handled with these params: ${JSON.stringify(receivedData)}`);
+	});
+}
+
+// Start the async server function
+startServer();
 
 // Handle shutdown signals
 process.on('SIGTERM', shutdown);
